f549459d958f6aca5c2429729819d6303fb41e2b
Tom Smale <smaletho@gmail.com>
2018-08-06 19:44:53 -0400

    moved all config stuff into one file instead of many

--- js/base.js
+++ js/base.js
@@ -7,3 +7,44 @@ $(function () {
 function firstLoadInit() {
     console.log('base.js loaded');
 }
+
+// called from validate.js
+function createTempList() {
+    var count = 0;
+    $(ConfigXml).contents().each(function processNodes() {
+        
+        
+
+
+        // if type is page, process it
+
+        if (this.nodeType == 3) {
+            var trimmed = $.trim(this.wholeText);
+            if (trimmed) {
+                // it's actually pure text
+            }
+        } else {
+            if (this.nodeName == "text") {
+                var str = "<div class='" + this.nodeName + "'>" + this.textContent + "</div>";
+                $("#tempXml").append(str);
+                $(this).contents().each(processNodes);
+            } else {
+                var str = "<div class='" + this.nodeName + "'>" + this.nodeName + "</div>";
+                $("#tempXml").append(str);
+                $(this).contents().each(processNodes);
+            }
+        }
+        
+    });
+}
+
+function getObject(element) {
+    
+    var ob = {
+        Id: $(element).prop('id'),
+        Type: element.nodeName,
+        Name: $(element).prop('name') || "",
+        Margin: 0
+    };
+
+}
