763f855011c725e774b238bde22db23a2f5f0bb9
smaletho <smaletho@gmail.com>
2018-08-08 17:04:44 -0400

    wow did I build a lot. I've got some navigation and TOC working.

--- js/base.js
+++ js/base.js
@@ -1,3 +1,7 @@
+
+var resizeTimer;
+
+
 $(function () {
     firstLoadInit();
     loadingInit();
@@ -5,14 +9,100 @@ $(function () {
 
     // load the file offline
     if (true) {
+        // when this function finishes, it will call the remaining init
         offlineLoadInit();
+    } else {
+        secondInit();
     }
 });
 
-function firstLoadInit() {
-    console.log('base.js loaded');
+function secondInit() {
+    buildTableOfContents();
+    initTocLinks();
+
+    // find the first page, and load it
+    //  eventually: check if they left off somewhere
+    loadPage();
 }
 
+function firstLoadInit() {
+    console.log('base.js loaded');
+
+
+    maintainAspectRatio();
+
+    $(window).on('resize', function (e) {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function () {
+            maintainAspectRatio();
+        }, 1000);
+    });
+
+
+    $("#menu-close").on('click', closeNav);
+    $("#menu-open").on('click', openNav);
+    $("#next-button").on('click', nextPage);
+    $("#previous-button").on('click', previousPage);
+}
+
+
+function initTocLinks() {
+    $(".nav-link").on('click', function () {
+        if ($(this).hasClass("chapter")) {
+            var chapterId = $(this).data('id');
+            var firstPage = $(ConfigXml).find("#" + chapterId).children("page").first().prop('id');
+            loadPage(firstPage);
+            closeNav();
+        } else {
+            var id = $(this).data('id');
+
+            // look for the plusMinus
+            var pM = $(this).find('.plusMinus').first();
+            if ($(pM).text() == "+") {
+                // expanding
+                $(pM).text("-");
+
+                // find class .sub-links with same data-id
+                $("#pageList").find('.sub-links[data-id=' + id + ']').first().css('display', 'block');
+            } else {
+                // collapsing
+                $(pM).text("+");
+
+                // find class .sub-links with same data-id
+                $("#pageList").find('.sub-links[data-id=' + id + ']').first().css('display', 'none');
+            }
+        }
+    });
+}
+
+
+
+
+function maintainAspectRatio() {
+    // the "17" is due to padding and stuff
+    var maxHeight = window.innerHeight - 18;
+    var maxWidth = window.innerWidth - 18;
+    var srcHeight = $("#main-window").height();
+    var srcWidth = $("#main-window").width();
+
+    ScaleRatio = calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight);
+    console.log("Set scale", ScaleRatio);
+
+    $("#main-window").css({
+        '-webkit-transform': 'scale(' + ScaleRatio + ')',
+        '-moz-transform': 'scale(' + ScaleRatio + ')',
+        '-ms-transform': 'scale(' + ScaleRatio + ')',
+        '-o-transform': 'scale(' + ScaleRatio + ')',
+        'transform': 'scale(' + ScaleRatio + ')'
+    });
+    
+}
+
+
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+
+    return Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+}
 
 
 //--------------------------------------
@@ -32,24 +122,15 @@ function firstLoadInit() {
 //
 //
 //--------------------------------------
-function processPages(pages) {
+function processPages() {
     // process the content of the pages in the xml
-    $(pages).each(function processNodes() {
 
-    });
-
-
-
-
-
-
+    
     $(ConfigXml).contents().each(function processNodes() {
 
-        if (blankTextNode(this)) {
-            console.log('skipped', $(this).prop('id'));
-        } else {
+        if (!blankTextNode(this)) {
             if (this.nodeName == "text") {
-                
+
                 var content = $(this).html().trim();
                 // text elements can contain children, and their html is presented differently
                 //      <text>
@@ -67,7 +148,7 @@ function processPages(pages) {
                         var div = "<div id='" + $(this).prop('id') + "' class='" + this.nodeName + "'>" + content + "</div>";
                         $("#tempXml").append(div);
                     }
-                    
+
 
                     $(this).contents().each(processNodes);
                 }
@@ -77,12 +158,8 @@ function processPages(pages) {
                 $(this).contents().each(processNodes);
             }
         }
+        
     });
-}
-
-function processTextElement(element) {
-    var text = $(this).html().trim();
-
 }
 
 
@@ -91,4 +168,73 @@ function buildTableOfContents() {
     // make ConfigXml into an easier to deal with object to generate the 
 
     console.log('all modules', $(ConfigXml).find('chapter').prop('id'))
+
+    // traverse the tree and build the TOC
+    var tocHtmlString = "";
+
+    $(ConfigXml).contents().each(function processNodes() {
+        // if it's the main book, render different
+        if (this.nodeName == "book") {
+            // fill in the title
+            $("#toc-book-title").text(this.attributes.name.value);
+
+            // recurse
+            $(this).contents().each(processNodes);
+        } else if (!blankTextNode(this) && this.nodeName != "page") {
+            var id = $(this).prop('id');
+            var type = this.nodeName;
+            var name = this.attributes.name.value;
+
+
+            tocHtmlString += "<div data-id='" + id + "' class='nav-link " + type + "'>";
+            tocHtmlString += "<div class='text'>" + name + "</div>";
+
+            
+            var children = $(this).children().length;
+            if (this.nodeName == "chapter") {
+                // if it's a chapter, update page numbers
+                tocHtmlString += "<div class='pageCounter'>1/" + children + "</div>";
+            } else if (children > 0) {
+                // else if has children, add plus sign
+                tocHtmlString += "<div class='plusMinus'>+</div>";
+            }
+
+
+            
+
+
+
+
+            tocHtmlString += "</div>";
+            tocHtmlString += "<div class='sub-links' data-id='" + id + "'>";
+
+            // recurse
+            $(this).contents().each(processNodes);
+
+            // close out the div
+            tocHtmlString += "</div>";
+        }
+    });
+
+    $("#pageList").html(tocHtmlString);
+
+    // subtracting 2 accounts for the border
+    $("#toc").height($("#inner-window").height() - 2);
+}
+
+
+
+
+function openNav() {
+    if ($("#toc").width() == 0) {
+        $("#toc").css('width', '33%');
+        $("#menu-open").css('margin-left', '33%');
+    } else {
+        closeNav();
+    }
+}
+
+function closeNav() {
+    $("#toc").width(0);
+    $("#menu-open").css('margin-left', '-1px');
 }
--- js/offline.js
+++ js/offline.js
@@ -1,15 +1,31 @@
 function offlineLoadInit() {
+    $("#offline-load").css('display', 'table');
+
     $("#config-file").on('change', function () {
         if ($("#config-file").val()) {
+
+            $("#loading").show();
+            updateLoadingText('Loading file');
+
             var file = $("#config-file").prop('files')[0];
             var fr = new FileReader();
             fr.onload = function () {
+                updateLoadingText('Received file, parsing data');
                 ConfigXml = $.parseXML(this.result);
                 processOfflineXml();
             };
+            fr.onerror = function () {
+                updateLoadingText('There was an error');
+            }
             fr.readAsText(file);
         }
     });
+}
+
+
+
+function updateLoadingText(text) {
+    $("#loading-text").text(text);
 }
 
 function processOfflineXml() {
@@ -19,31 +35,49 @@ function processOfflineXml() {
     $(ConfigXml).contents().each(function processNodes() {
         
         // check for empty node
-        if (blankTextNode(this)) {
-            console.log('skipped', $(this).prop('id'));
-        } else {
+        if (!blankTextNode(this)) {
             if (this.nodeName == "page") {
                 // I'm cutting out all the stuff page contents when it can be sent from the server
                 var id = $(this).prop('id');
 
-                PageContent.push(this.cloneNode());
+                var parentChapter = $(this).parent("chapter");
+                var parentSection = $(parentChapter).parent("section");
+                var parentModule = $(parentSection).parent("module");
+
+                PageContent.push({
+                    Module: $(parentModule).prop('id'),
+                    Section: $(parentSection).prop('id'),
+                    Chapter: $(parentChapter).prop('id'),
+                    Page: id,
+                    content: this.cloneNode(true),
+                });
 
                 // make an empty page piece
                 var blankPage = document.createElement("page");
                 $(blankPage).prop('id', id);
                 this.parentNode.replaceChild(blankPage, this);
-                                
+
             }
 
             // recurse
             $(this).contents().each(processNodes);
         }
-
     });
+
+    updateLoadingText('Finished processing book');
+
+    setTimeout(function () {
+        updateLoadingText('');
+        $("#loading").hide();
+        $("#offline-load").css('display', 'none');
+        $("#main-window").css('display', 'table');
+
+        secondInit();
+    }, 2000);
 
     // process the pages now
     //  to: base.js
-    processPages(PageContent);
+    //processPages(PageContent);
 
-    buildTableOfContents();
+    
 }
